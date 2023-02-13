31b29998094addf752382f6e9bef0a3563104f0a
Andrii Beznosov <andrey301288@gmail.com>
2023-02-13 03:30:39 +0200

    test

--- validation/auth.js
+++ validation/auth.js
@@ -2,10 +2,12 @@ import { body } from "express-validator";
 
 export const registerValidation = [
   body("email", "Не вірний формат пошти").isEmail(),
+
   body("password", "Пароль повинен бути мінимум 5 символів").isLength({
     min: 5,
   }),
   body("fullName", "Вкажіть 'і'мя' не менше 3 символів").isLength({ min: 3 }),
+
   body("avatarUrl", "Не вірний формат посилання до картинки")
     .optional()
     .isURL(),
